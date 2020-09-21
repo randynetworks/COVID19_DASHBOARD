7f52fc184a192112f1cbe2a2eca69e10c78f2a8a
randynetworks <randynetworks@gmail.com>
2020-09-21 11:57:26 +0700

    fix ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NationalComponent } from './national/national.component';
+import { NewsComponent } from './news/news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/news',
+    pathMatch: 'full'
+  },
+  {
+    path: 'news',
+    component: NewsComponent
+  },
+  {
+    path: 'national',
+    component: NationalComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { NationalComponent } from './national/national.component';
+import { NewsComponent } from './news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NationalComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
